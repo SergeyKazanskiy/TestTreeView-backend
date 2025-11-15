7a6e81e894fa2afaead9e861cfe59a46049bf261
Sergey Kazanskiy <sergeykazanskiy74@gmail.com>
2025-11-15 21:25:40 +0200

    Create login

--- app/auth/routers.py
+++ app/auth/routers.py
@@ -1,52 +1,70 @@
 from fastapi import APIRouter, HTTPException, Depends
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 from database import get_session
 from schemas.user import UserCreate, UserLogin, RegistrationResponse, LoginResponse
 from models.users import User
-from auth.utils import get_custom_token, validate_email, validate_password 
+from auth.utils import validate_email, validate_password
+import firebase_admin
+from firebase_admin import auth, credentials
+from pathlib import Path
 #import bcrypt
+
+
+firebase_key_path = Path("../secrets/firebase-key.json")
+if not firebase_key_path.is_file():
+    raise RuntimeError(f"{firebase_key_path} not found")
+
+cred = credentials.Certificate(str(firebase_key_path))
+firebase_admin.initialize_app(cred)
 
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 @router.post("/register", response_model=RegistrationResponse)
-def register_user(user_data: UserCreate, db: Session = Depends(get_session)):
+async def register_user(user_data: UserCreate, db: AsyncSession = Depends(get_session)):
     validate_email(user_data.email)
     validate_password(user_data.password)
 
-    existing_user = db.query(User).filter(User.email == user_data.email).first()
+    result = await db.execute(select(User).filter(User.email == user_data.email))
+    existing_user = result.scalar_one_or_none()
     if existing_user:
         raise HTTPException(status_code=400, detail="Email already registered")
 
     #hashed_password = bcrypt.hashpw(user_data.password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
 
     new_user = User(
-        name=user_data.name,
+        first_name=user_data.first_name,
+        last_name=user_data.last_name,
         email=user_data.email,
-        password=user_data.password #hashed_password
+        password=user_data.password, #hashed_password
+        token_FCM=""
     )
     db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+    await db.commit()
+    await db.refresh(new_user)
 
-    token = get_custom_token(f"user_{new_user.id}")
+    token = auth.create_custom_token(f"user_{new_user.id}")
     return {"user_id": new_user.id, "token": token}
 
 
-@router.post("/login", response_model=LoginResponse)
-def login_user(user_data: UserLogin, db: Session = Depends(get_session)):
+@router.post("/login")
+async def login_user(user_data: UserLogin, db: AsyncSession = Depends(get_session)):
     validate_email(user_data.email)
     validate_password(user_data.password)
 
-    user = db.query(User).filter(User.email == user_data.email).first()
+    query = select(User).where(User.email == user_data.email)
+    result = await db.execute(query)
+    user = result.scalar_one_or_none()
+
     if not user:
-        raise HTTPException(status_code=400, detail="Invalid credentials")
+        raise HTTPException(status_code=400, detail="No account found with this email")
 
     # if not bcrypt.checkpw(user_data.password.encode("utf-8"), user.password.encode("utf-8")):
-    if not user or user.password != user_data.password:
-        raise HTTPException(status_code=400, detail="Invalid credentials")
-
-    token = get_custom_token(f"user_{user.id}")
+    if user.password != user_data.password:
+        raise HTTPException(status_code=400, detail="Invalid password")
+    
+    token = auth.create_custom_token(f"user_{user.id}")
     return {"user_id": user.id, "token": token, "first_name": user.first_name, "last_name": user.last_name}
 
 
--- app/auth/utils.py
+++ app/auth/utils.py
@@ -1,16 +1,7 @@
 from fastapi import Header, HTTPException, status
-from .setup import firebase_auth
 import re
-
-
-EMAIL_REGEX = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-PASSWORD_REGEX = (
-    r"^(?=.*[A-Z])"      # хотя бы одна заглавная буква
-    r"(?=.*[a-z])"       # хотя бы одна строчная буква
-    r"(?=.*\d)"          # хотя бы одна цифра
-    r"(?=.*[@$!%*?&])"   # хотя бы один спецсимвол
-    r"[A-Za-z\d@$!%*?&]{8,}$"  # минимум 8 символов
-)
+from firebase_admin import auth
+from .constants import EMAIL_REGEX, PASSWORD_REGEX
 
 
 def validate_email(email: str):
@@ -28,26 +19,13 @@ def validate_password(password: str):
             ),
         )
     
-
-async def get_custom_token(user_id: str) -> str:
-    try:
-        token_bytes = firebase_auth.create_custom_token(user_id)
-        if isinstance(token_bytes, bytes):
-            token = token_bytes.decode("utf-8")
-        else:
-            token = token_bytes
-        return token
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to create custom token")
-
-
 async def get_decoded_token(authorization: str = Header(None)) -> dict:
     if not authorization or not authorization.startswith("Bearer "):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Missing or invalid Authorization header")
 
     id_token = authorization.split("Bearer ")[1]
     try:
-        decoded_token = firebase_auth.verify_id_token(id_token)
+        decoded_token = auth.verify_id_token(id_token)
         return decoded_token
     except Exception:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Firebase ID token")
